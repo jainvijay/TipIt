1a7ed2c3311062a78603c59cbaece778276f3863
Vijayjain <jvijay@yahoo-inc.com>
2014-09-04 19:09:22 -0700

    Initial Commit

---
+++ Objective-C Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Objective-C Example
+//
+//  Created by Vijay Jain on 9/4/14.
+//  Copyright (c) 2014 Vijay Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
